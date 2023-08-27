491800766e0c2c1f5efde78d935de08bf4bf528f
Torata-Inoue <i_am_torao@yahoo.co.jp>
2023-08-27 16:22:11 +0900

    42 基本的なルーティング

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,16 +4,19 @@ import SignIn from "./components/SignIn.tsx";
 import SignUp from "./components/SignUp.tsx";
 import Main from "./components/Main.tsx";
 import NotFound from "./components/NotFound.tsx";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
 
   return (
-    <div className="App">
-      <SignIn />
-      <SignUp />
-      <Main />
-      <NotFound />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="signin" element={<SignIn />} />
+        <Route path="signup" element={<SignUp />} />
+        <Route path="/" element={<Main />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
