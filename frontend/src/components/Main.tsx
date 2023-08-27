e2193f7bee4100cd86439d6fd8c3c502fc9a42c2
Torata-Inoue <i_am_torao@yahoo.co.jp>
2023-08-28 08:58:42 +0900

    52 タスク一覧のレイアウト作成

--- frontend/src/components/Main.tsx
+++ frontend/src/components/Main.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Header from "./Header.tsx";
+import TaskTable from "./TaskTable.tsx";
 
 const Main: React.FC = () => {
   return (
     <>
       <Header />
-      <div>main</div>
+      <TaskTable />
     </>
   )
 }
