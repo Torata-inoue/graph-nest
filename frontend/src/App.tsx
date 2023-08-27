ba9177aeaae6dbe039e8e015217ce80ad57a72fc
Torata-Inoue <i_am_torao@yahoo.co.jp>
2023-08-27 17:07:49 +0900

    45 GraphQLクライアントの作成

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,18 +6,22 @@ import Main from "./components/Main.tsx";
 import NotFound from "./components/NotFound.tsx";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import {GuestRoute, PrivateRoute} from "./AppRoute.tsx";
+import client from "./apolloClient.ts";
+import {ApolloProvider} from "@apollo/client";
 
 function App() {
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="signin" element={<GuestRoute><SignIn /></GuestRoute>} />
-        <Route path="signup" element={<GuestRoute><SignUp /></GuestRoute>} />
-        <Route path="/" element={<PrivateRoute><Main /></PrivateRoute>} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="signin" element={<GuestRoute><SignIn /></GuestRoute>} />
+          <Route path="signup" element={<GuestRoute><SignUp /></GuestRoute>} />
+          <Route path="/" element={<PrivateRoute><Main /></PrivateRoute>} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </ApolloProvider>
   )
 }
 
