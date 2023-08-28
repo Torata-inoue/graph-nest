98cd66237b08a50ae1afdefd99f1d56b82d3cca0
Torata-Inoue <i_am_torao@yahoo.co.jp>
2023-08-28 09:18:33 +0900

    53 タスク一覧取得機能の実装

--- frontend/src/components/Main.tsx
+++ frontend/src/components/Main.tsx
@@ -1,8 +1,21 @@
 import React from "react";
 import Header from "./Header.tsx";
 import TaskTable from "./TaskTable.tsx";
+import jwtDecode from "jwt-decode";
+import {Payload} from "../types/payload.ts";
+import {useQuery} from "@apollo/client";
+import {Task} from "../types/task.ts";
+import {GET_TASKS} from "../queries/taskQueries.ts";
 
 const Main: React.FC = () => {
+  const token = localStorage.getItem('token');
+  const decodedToken = jwtDecode<Payload>(token!);
+  const userId = decodedToken.sub;
+
+  const {loading, data, error} = useQuery<{getTasks: Task[]}>(GET_TASKS, {
+    variables: {userId}
+  });
+  console.log(data);
   return (
     <>
       <Header />
