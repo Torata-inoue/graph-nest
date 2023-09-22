1c0b805e96304b4b4a4685471b3ec87e2fc874b8
Torata-Inoue <i_am_torao@yahoo.co.jp>
2023-09-22 16:58:23 +0900

    migration

--- backend/prisma/migrations/20230922074544_init/migration.sql
+++ backend/prisma/migrations/20230922074544_init/migration.sql
@@ -2,13 +2,21 @@
 CREATE TABLE `Task` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(255) NOT NULL,
-    `dueDate` VARCHAR(10) NOT NULL,
-    `status` ENUM('NOT_STARTED', 'IN_PROGRESS', 'COMPLETED') NOT NULL DEFAULT 'NOT_STARTED',
-    `description` VARCHAR(191) NULL,
+    `body` TEXT NOT NULL,
+    `userId` INTEGER NOT NULL,
+    `isTask` BOOLEAN NOT NULL,
+    `to` TEXT NOT NULL,
+    `dueTime` VARCHAR(10) NOT NULL,
+    `isEveryday` BOOLEAN NOT NULL,
+    `dayOfWeek` INTEGER NULL,
+    `date` INTEGER NULL,
     `createdAt` TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
     `updatedAt` TIMESTAMP(0) NOT NULL,
-    `userId` INTEGER NOT NULL,
 
+    INDEX `Task_userId_createdAt_idx`(`userId`, `createdAt`),
+    INDEX `Task_date_idx`(`date`),
+    INDEX `Task_isEveryday_idx`(`isEveryday`),
+    INDEX `Task_dayOfWeek_idx`(`dayOfWeek`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
