914824ea5e55ef8349342e59967bfd6a5230c83f
Torata-inoue <i_am_torao@yahoo.co.jp>
2023-09-26 14:42:49 +0900

    タスク発行処理作成

--- backend/src/send-message/send-message.service.ts
+++ backend/src/send-message/send-message.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
 import { TaskService } from '../task/task.service';
 import { ChatworkService } from '../chatwork/chatwork.service';
+import { Task } from '@prisma/client';
 
 @Injectable()
 export class SendMessageService {
@@ -13,18 +14,32 @@ export class SendMessageService {
   // @Cron('* 30 * * * *')
   // @Cron('1 * * * * *')
   async handleCron(): Promise<void> {
-    // const now = new Date();
-    // const tasks = await this.taskService.getSendTasks(
-    //   now.getHours(),
-    //   now.getDay(),
-    //   now.getDate(),
-    // );
-    //
-    // tasks.map((task) => {
-    //   const toIds: number[] = JSON.parse(task.to);
-    //   this.chatworkService.postMessage(task.roomId, toIds, task.body);
-    // });
+    const now = new Date();
+    const tasks = await this.taskService.getSendTasks(
+      now.getHours(),
+      now.getDay(),
+      now.getDate(),
+    );
 
-    this.chatworkService.getRoomMembers(236136302);
+    tasks.map((task: Task) => {
+      // TODO toIdsを配列化
+      const toIds = task.to;
+      if (task.isTask) {
+        const limit = this.getLimit(task, now);
+        this.chatworkService.postTask(task.roomId, toIds, task.body, limit);
+        return;
+      }
+
+      this.chatworkService.postMessage(task.roomId, toIds, task.body);
+    });
+  }
+
+  getLimit(task: Task, now: Date): number {
+    const limit =
+      now.getTime() +
+      task.limitDate * 24 * 60 * 60 * 1000 +
+      task.limitHour * 60 * 60 * 1000;
+
+    return Math.floor(limit) / 1000;
   }
 }
