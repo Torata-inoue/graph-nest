e2c4fdfbac844fdacd6897fd8a75dde85609ecb2
Torata-inoue <i_am_torao@yahoo.co.jp>
2023-10-03 17:39:49 +0900

    TaskDetail

--- frontend/src/components/private/action/panes/TaskDetail.tsx
+++ frontend/src/components/private/action/panes/TaskDetail.tsx
@@ -6,32 +6,43 @@ import DialogActions from "@mui/material/DialogActions";
 import Button from "@mui/material/Button";
 import { TaskPanesProps } from "./TaskPaneRouter.tsx";
 import { ROUTE_NUM } from "../../../../types/routeNum.ts";
+import InlineTextInput from "../../../inputs/InlineTextInput.tsx";
+import MultilineInput from "../../../inputs/MultilineInput.tsx";
 
 const TaskDetail: React.FC<TaskPanesProps> = ({
   title,
   setRouteNum,
   formMethods,
 }) => {
-  const {} = formMethods;
+  const {register, formState: {errors}, trigger} = formMethods;
+  const handleClickNext = async () => {
+    const isValidName = await trigger('name');
+    const isValidBody = await trigger('body');
+    if (!isValidName && !isValidBody) {
+      alert('タイトル、本文を入力してください');
+      return;
+    }
+    setRouteNum(ROUTE_NUM.DUE_TIME);
+  }
   return (
     <>
       <DialogTitle>{title}</DialogTitle>
       <DialogContent>
         <FormControl>
           <FormLabel>タスクの詳細</FormLabel>
-          タスクのタイトル
-          <input type="text" />
-          メッセージ内容 <textarea name="" id=""></textarea>
+          <InlineTextInput placeholder="タスクタイトル" register={register('name')} error={errors.name} />
+          <FormLabel>メッセージ本文</FormLabel>
+          <MultilineInput placeholder="メッセージ本文" register={register('body')} error={errors.body} />
         </FormControl>
         <DialogActions>
           <Button onClick={() => setRouteNum(ROUTE_NUM.SELECT_MENTION)}>
             戻る
           </Button>
-          <Button onClick={() => setRouteNum(ROUTE_NUM.DUE_TIME)}>次へ</Button>
+          <Button onClick={handleClickNext}>次へ</Button>
         </DialogActions>
       </DialogContent>
     </>
-  );
+  )
 };
 
 export default TaskDetail;
