067f7f993afface4d8c1629af58a8b9c0f4d4748
Torata-inoue <i_am_torao@yahoo.co.jp>
2023-10-12 16:11:45 +0900

    更新の送信部分

--- frontend/src/hooks/private/useEditTask.ts
+++ frontend/src/hooks/private/useEditTask.ts
@@ -1,18 +1,24 @@
 import { useForm, UseFormReturn } from "react-hook-form";
 import { TaskInputType } from "./useAddTask.ts";
+import { Task } from "../../types/task.ts";
 
-export const useEditTask: (userId: number) => UseFormReturn<TaskInputType> = (
-  userId,
+export const useEditTask: (task: Task) => UseFormReturn<TaskInputType> = (
+  task,
 ) => {
   return useForm<TaskInputType>({
     defaultValues: {
-      name: "",
-      body: "",
-      userId,
-      isTask: false,
-      to: "",
-      dueTime: 10,
-      isEveryday: false,
+      name: task.name,
+      body: task.body,
+      userId: task.userId,
+      isTask: task.isTask,
+      to: task.to,
+      dueTime: task.dueTime,
+      isEveryday: task.isEveryday,
+      dayOfWeek: task.dayOfWeek,
+      date: task.date,
+      roomId: task.roomId,
+      limitDate: task.limitDate,
+      limitHour: task.limitHour,
     },
   });
 };
