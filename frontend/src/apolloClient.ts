e19941723d21cc21b7f882ae064fc992307cbd7f
Torata-inoue <i_am_torao@yahoo.co.jp>
2023-10-20 19:25:11 +0900

    appoloURL変更

--- frontend/src/apolloClient.ts
+++ frontend/src/apolloClient.ts
@@ -2,7 +2,7 @@ import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:3000/graphql",
+  uri: import.meta.env.VITE_APPOLO_URL,
 });
 
 const authLink = setContext((_, prevContext) => {
